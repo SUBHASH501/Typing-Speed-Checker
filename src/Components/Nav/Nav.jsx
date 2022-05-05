8f791adfd729acacc7fdccc28b9c07828d148161
Subhash Kumar <subhashishwar001@gmail.com>
2022-05-05 14:00:41 +0530

    Typing Speed in React js

---
+++ src/Components/Nav/Nav.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import logo from './../../assests/logo.png'
+import './Nav.css';
+
+export default function Nav() {
+  return (
+      <div className="nav-container">
+          <div className="nav-left">
+               <img className='flash-logo'  src={logo} alt="logo" />
+               <p className="flash-logo-text">FlashType</p>
+          </div>
+          <div className="nav-right">
+              <a href="https://www.linkedin.com/in/subhashkumar501/"
+                  target="_blank"  
+                  className='nav-name-link'
+                  rel='noreferrer'
+                  >Subhash Kumar</a>
+          </div>
+      </div>
+  )
+}
